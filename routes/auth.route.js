6a8b26894b1a35bf029dff7ffeca9fdb2aef8166
DevAhmed <154789805+AhmedLyanov@users.noreply.github.com>
2025-10-30 22:41:30 +0300

    add validation

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -2,19 +2,23 @@ import { Router } from "express";
 import authController from "../controllers/auth.controller.js";
 import upload from "../config/multer.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
+import {
+  validateRegistration,
+  validateLogin,
+} from "../middlewares/validators/auth.validator.js";
 
 const authRouter = Router();
 
-authRouter.post('/registration', upload.single('avatar'), authController.registration);
-authRouter.post('/login', authController.login);
+authRouter.post(
+  "/registration",
+  upload.single("avatar"),
+  validateRegistration,
+  authController.registration
+);
+authRouter.post("/login", validateLogin, authController.login);
 
-// getUsersRole
-authRouter.get('/role/admin', authController.getUsersRolesAdmin);
-authRouter.get('/role/user', authController.getUsersRolesUser);
-authRouter.get('/role/student', authController.getUsersRolesStudent);
-
-//getProfile
-authRouter.get('/user/profile', authMiddleware, authController.getUserProfile);
-
+authRouter.get("/role/admin", authController.getUsersRolesAdmin);
+authRouter.get("/role/user", authController.getUsersRolesUser);
+authRouter.get("/role/student", authController.getUsersRolesStudent);
 
 export default authRouter;
