033be6c3744e3c67c7a415406f14b09a9f1fe251
Ведзижев Зураб Б <VedzizhevZB23@ithub.local>
2025-11-11 15:35:18 +0300

    Реализована проверка на авторизацию

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 const router = new Router();
 import projectController from "../controllers/projectController.js";
-// import authMiddleware from "../middlewares/authMiddleware.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
 import upload from '../middlewares/project-upload.js';
 
 router.get("/allProjects", projectController.getProjects);
-router.get("/getProject/:id", projectController.getOneProject);
-router.post("/addProject", upload.array('images', 12), projectController.addProject);
-router.delete("/delProject/:id", projectController.deleteOneProject);
-router.patch("/update/:id", projectController.updateProject);
-router.get("/user-projects/:userId", projectController.getUserProjects);
+router.get("/getProject/:id", authMiddleware, projectController.getOneProject);
+router.post("/addProject", authMiddleware, upload.array('images', 12), projectController.addProject);
+router.delete("/delProject/:id", authMiddleware, projectController.deleteOneProject);
+router.patch("/update/:id", authMiddleware, projectController.updateProject);
+router.get("/user-projects/:userId", authMiddleware, projectController.getUserProjects);
 
 export default router;
